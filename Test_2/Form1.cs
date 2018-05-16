d7926e390f4151b3e49fe7b6ecccb46a52af3af4
artstesh <artstesh@mail.ru>
2018-05-16 20:11:37 +0300

    refactoring

--- Test_2/Form1.cs
+++ Test_2/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 using Test_2.Models;
 using Test_2.Tools;
@@ -8,28 +9,34 @@ namespace Test_2
     
     public partial class Form1 : Form
     {
-        private Animal viewAnOne;
-        private Animal viewAnTwo;
+        private Animal[] _animals = new Animal[2];
 
         private void Analize ()
-        {   
-            if (viewAnOne.massa > viewAnTwo.massa) tbAnalize.Text = viewAnOne.name + " весит больше, чем " + viewAnTwo.name;
-            if (viewAnOne.massa < viewAnTwo.massa) tbAnalize.Text = viewAnOne.name + " весит меньше, чем " + viewAnTwo.name;
-            if (viewAnOne.massa == viewAnTwo.massa) tbAnalize.Text = "животные весят одинаково";
+        {
+            if (cbOne.SelectedItem == null && cbUnit.SelectedItem == null || _animals.Any(e => e == null)) return;
+            var animals = _animals.OrderBy(e => e.massa).ToArray();
+            if (animals[0].massa == animals[1].massa)
+            {
+                tbAnalize.Text = "животные весят одинаково";
+                return;
+            }
+            tbAnalize.Text = animals[1].name + " весит больше, чем " + animals[0].name;
         }
 
         private void ChangeUnit1 ()
         {
-            viewAnOne.unit = cbUnit.SelectedItem.ToString();
-            viewAnOne.massaGen = MassConverter.ConvertKilo(viewAnOne.massa, viewAnOne.unit);
-            tbOne.Text = viewAnOne.view();
+            if (_animals[0] == null || cbUnit.SelectedItem == null) return;
+            _animals[0].unit = cbUnit.SelectedItem.ToString();
+            _animals[0].massaGen = MassConverter.ConvertKilo(_animals[0].massa, _animals[0].unit);
+            tbOne.Text = _animals[0].view();
         }
 
         private void ChangeUnit2 ()
         {
-            viewAnTwo.unit = cbUnit.SelectedItem.ToString();
-            viewAnTwo.massaGen = MassConverter.ConvertKilo(viewAnTwo.massa, viewAnTwo.unit);
-            tbTwo.Text = viewAnTwo.view();
+            if (_animals[1] == null || cbUnit.SelectedItem == null) return;
+            _animals[1].unit = cbUnit.SelectedItem.ToString();
+            _animals[1].massaGen = MassConverter.ConvertKilo(_animals[1].massa, _animals[1].unit);
+            tbTwo.Text = _animals[1].view();
         }
 
         public Form1()
@@ -46,23 +53,23 @@ namespace Test_2
 
         private void cbOne_SelectedIndexChanged(object sender, EventArgs e)
         {
-            viewAnOne = AnimalFactory.Create(cbOne.SelectedItem.ToString());
-            if (cbUnit.SelectedItem != null) ChangeUnit1(); else tbOne.Text = "***Не выбраны единицы веса***";
-            if (cbTwo.SelectedItem != null && cbUnit.SelectedItem != null) Analize();
+            _animals[0] = AnimalFactory.Create(cbOne.SelectedItem.ToString());
+            if (cbUnit.SelectedItem == null) tbOne.Text = "***Не выбраны единицы веса***";
+            Analize();
         }
 
         private void cbTwo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            viewAnTwo = AnimalFactory.Create(cbTwo.SelectedItem.ToString());
-            if (cbUnit.SelectedItem != null) ChangeUnit2(); else tbTwo.Text = "***Не выбраны единицы веса***";
-            if (cbOne.SelectedItem != null && cbUnit.SelectedItem != null) Analize();            
+            _animals[1] = AnimalFactory.Create(cbTwo.SelectedItem.ToString());
+            if (cbUnit.SelectedItem == null) tbTwo.Text = "***Не выбраны единицы веса***";
+            Analize();            
         }
 
         private void cbUnit_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cbOne.SelectedItem != null) ChangeUnit1();
-            if (cbTwo.SelectedItem != null) ChangeUnit2();
-            if (cbOne.SelectedItem != null && cbTwo.SelectedItem != null) Analize();
+            ChangeUnit1();
+            ChangeUnit2();
+            Analize();
         }
     }
 }
