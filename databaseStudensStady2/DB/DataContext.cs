fcd9e559ee1a73f89191f55a3ecdc30157b5b420
artstesh <artstesh@mail.ru>
2018-05-28 07:02:57 +0300

    StudyDb is added

---
+++ databaseStudensStady2/DB/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using databaseStudensStady2.Models;
+
+namespace databaseStudensStady2.DB
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DataContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
